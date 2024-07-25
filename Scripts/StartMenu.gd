21287ef162f84e00e8dcaa4da9f15bcbc95321c5
LoganAmes <bobcorevette@gmail.com>
2024-07-24 23:42:13 -0500

    Adding Alchemy Table UI, Making Main Menu Pretty
    Adding Alchemy Table UI, Making Main Menu Pretty

--- Scripts/StartMenu.gd
+++ Scripts/StartMenu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Main_Scenes/AlchemyChamber.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Main_Scenes/DeskChamber.tscn")
 
 
 func _on_quit_pressed():
